b719d65f37bd94e6b5bbf5a08fddfb347cca2bba
Tatiana <tanja6122212@list.ru>
2021-12-18 18:40:11 +0900

    Initial commit

---
+++ src/main/java/pro/sky/skyproexceptionswork/SkyproExceptionsWorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproexceptionswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproExceptionsWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproExceptionsWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyproexceptionswork/SkyproExceptionsWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproexceptionswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproExceptionsWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
