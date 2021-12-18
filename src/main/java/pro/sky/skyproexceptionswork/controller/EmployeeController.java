954709c9a7c12a4c92ba91f52f7f2f2b55746773
Tatiana <tanja6122212@list.ru>
2021-12-19 00:48:01 +0900

    there is an error after run

---
+++ src/main/java/pro/sky/skyproexceptionswork/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package pro.sky.skyproexceptionswork.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.skyproexceptionswork.service.EmployeeServiseImpl;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeServiseImpl employeeService;
+
+    public EmployeeController(EmployeeServiseImpl employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(path = "/add")
+    public String add(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
+        employeeService.add(firstName, lastName);
+        return "Сотрудник " + firstName + lastName + " успешно создан.";
+    }
+
+    @GetMapping(path = "/remove")
+    public String remove(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
+        employeeService.remove(firstName, lastName);
+        return "Сотрудник " + firstName + " " + lastName + " успешно удалён.";
+    }
+
+    @GetMapping(path = "/find")
+    public String find(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
+        employeeService.remove(firstName, lastName);
+        return "Сотрудник: " + firstName + " " + lastName;
+    }
+}
---
+++ src/main/java/pro/sky/skyproexceptionswork/service/EmployeeServiseImpl.java
@@ -0,0 +1,60 @@
+package pro.sky.skyproexceptionswork.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.skyproexceptionswork.Employee;
+import pro.sky.skyproexceptionswork.EmployeeServise;
+import pro.sky.skyproexceptionswork.exception.EmployeeCannotBeAddedException;
+import pro.sky.skyproexceptionswork.exception.EmployeeExistsException;
+import pro.sky.skyproexceptionswork.exception.NoEmployeeFoundException;
+
+@Service
+public class EmployeeServiseImpl implements EmployeeServise {
+
+    Employee[] employees = new Employee[10];
+
+    @Override
+    public Employee add(String firstName, String lastName) {
+        Employee newEmployee = new Employee(firstName, lastName);
+        if (indexOf(newEmployee) != -1) {
+            throw new EmployeeExistsException();
+        }
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                employees[i] = newEmployee;
+                return newEmployee;
+            }
+        }
+        throw new EmployeeCannotBeAddedException();
+    }
+
+    private int indexOf(Employee newEmployee) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].equals(newEmployee)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public Employee remove(String firstName, String lastName) {
+        Employee newEmployee = new Employee(firstName, lastName);
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].equals(newEmployee)) {
+                employees[i] = null;
+                return employees[i];
+            }
+        }
+        throw new NoEmployeeFoundException();
+    }
+
+    @Override
+    public Employee find(String firstName, String lastName) {
+        Employee newEmployee = new Employee(firstName, lastName);
+        if (indexOf(newEmployee) != -1) {
+            return newEmployee;
+        }
+        throw new NoEmployeeFoundException();
+    }
+
+}
